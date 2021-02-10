567494b389c9ea648a0462b5c29e22973b3e115d
cyrus25 <cyruspassi@gmail.com>
2021-02-10 12:10:15 +0530

    used join in controller

--- src/Controllers/ShareDialogController.php
+++ src/Controllers/ShareDialogController.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Auth;
 use Silber\Bouncer\Bouncer as BouncerBouncer;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
+use Exception;
+use Silber\Bouncer\Database\Queries\Abilities;
+use Illuminate\Support\Facades\DB;
+
+
 
 class ShareDialogController extends Controller
 {
@@ -23,96 +28,126 @@ class ShareDialogController extends Controller
     public function showShareDialog($entity, $entityId)
     {
 
-        $entityCapitalize = ucfirst($entity);
-        $entityModel = substr($entityCapitalize, 0, -1);
-        $model_name = 'App\Models\\' . $entityModel;
-        if (class_exists("App\Models\\" . $entityModel)) {
-            $authUser = Auth::user();
-            $model = $model_name::with('user')->findOrFail($entityId);
-            $users = User::with('abilities')->get();
+        try {
+            $entityCapitalize = ucfirst($entity);
+            $entityModel = substr($entityCapitalize, 0, -1);
+            $model_name = 'App\Models\\' . $entityModel;
+            $entityModelSmall = substr($entity, 0, -1);
+            if (class_exists("App\Models\\" . $entityModel)) {
 
-            if ($authUser->id == $model->user_id) {
-                Bouncer::allow($authUser)->toOwn($model_name);
-                Bouncer::allow($authUser)->to('write', $model_name);
-                Bouncer::allow($authUser)->to('read', $model_name);
-            }
 
-            $validUsers = [];
-            foreach ($users as $user) {
-                foreach ($user->abilities as $ability) {
-                    if ($ability->entity_type == $model_name && $ability->entity_id == $model->id && $user->id != $authUser->id) {
-                        if ($ability->name == "read")
-                            $user->ability = "Read";
-                        else if ($ability->name = "write")
-                            $user->ability = "Can Edit";
-                        array_push($validUsers, $user);
+                $authUser = Auth::user();
+
+                //find model
+                $model = $model_name::with('user')->findOrFail($entityId);
+                $model['entity_name'] = $entityModelSmall;
+
+
+                //if auth user does not own model
+                if ($model->user_id != $authUser->id) {
+                    return back()->withErrors("You are not authorized!");
+                }
+
+                //else assign ability to auth user
+                if ($authUser->id == $model->user_id) {
+                    Bouncer::allow($authUser)->toOwn($model);
+                    Bouncer::allow($authUser)->to('write', $model);
+                    Bouncer::allow($authUser)->to('read', $model);
+                }
+
+                //join on permission and abilities
+                //returns user_id and ability of valid users on model
+                $users = DB::table('abilities')
+                    ->select('abilities.name', 'permissions.entity_id')
+                    ->join('permissions', function ($join) use ($model) {
+                        $join->on('abilities.id', '=', 'permissions.ability_id')
+                            ->where('abilities.entity_id', $model->id);
+                    })
+                    ->get();
+
+
+                //find user according to id and assign permission to user object
+                $invitedUsers = [];
+                $mapArray = array();
+                foreach ($users as $index => $key) {
+                    if ($key->entity_id != $model->user_id) {
+                        $key->name = ($key->name == "read") ? "Read" : "Can Edit";
+                        $mapArray[$key->entity_id] = $key->name;
+                        array_push($invitedUsers, $key->entity_id);
                     }
                 }
+                $users = User::find($invitedUsers);
+
+                foreach ($users as $user) {
+                    $user->ability = $mapArray[$user->id];
+                }
+                return Inertia::render('ShareDialog/index', ['entity' => $model, 'users' => $users]);
+            } else {
+
+                return back()->withErrors("Model does not exist");
             }
-
-            if ($model->user_id != $authUser->id) {
-                return back()->withErrors("You are not authorized!");
-            }
-
-            $entityModelSmall = substr($entity, 0, -1);
-            $model['entity_name'] = $entityModelSmall;
-
-            return Inertia::render('ShareDialog/index', ['entity' => $model, 'users' => $validUsers]);
-        } else {
-            return back()->withErrors("Model does not exist");
+        } catch (\Exception $e) {
+            return back()->withErrors($e->getMessage());
         }
     }
 
 
     public function createUser($email)
     {
-        $user = User::create([
-            'name' => "User",
-            'email' => $email,
-            'password' => Hash::make('user'),
-        ]);
+        try {
+            $user = User::create([
+                'name' => "User",
+                'email' => $email,
+                'password' => Hash::make('user'),
+            ]);
 
-        return $user;
+            return $user;
+        } catch (\Exception $e) {
+            return back()->withErrors($e->getMessage());
+        }
     }
+
 
     public function assignAbility(Request $request)
     {
-        $validatedData = $request->validate([
-            'emails' => 'required',
-            'emails.*.email' => 'bail|required|email|min:5',
-            'ability' => 'required'
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'emails' => 'required',
+                'emails.*.email' => 'bail|required|email|min:5',
+                'ability' => 'required'
+            ]);
 
-        $entityClass = ucfirst($request->entity_name);
-        $model_name = 'App\Models\\' . $entityClass;
-        $model = $model_name::findOrFail($request->entity_id);
-        $entityModelSmall = $request->entity_name . 's';
+            $entityClass = ucfirst($request->entity_name);
+            $model_name = 'App\Models\\' . $entityClass;
+            $model = $model_name::findOrFail($request->entity_id);
+            $entityModelSmall = $request->entity_name . 's';
 
 
-        $message = "User Access Changed Successfully!";
-        $emails = $request->emails;
-        $newUser = false;
-        foreach ($emails as $email) {
+            $message = "User Access Changed Successfully!";
+            $emails = $request->emails;
+            foreach ($emails as $email) {
 
-            $user = User::where('email', $email['email'])->first();
-            if (!$user) {
-                $user = $this->createUser($email['email']);
-                if (!$user)
-                    return back()->withErrors("User could not be created successfully!");
-                $message = "Users Invited!";
+                $user = User::where('email', $email['email'])->first();
+                if (!$user) {
+                    $user = $this->createUser($email['email']);
+                    if (!$user)
+                        return back()->withErrors("User could not be created successfully!");
+                    $message = "Users Invited!";
+                }
+
+                $validAbilities = ['read', 'write'];
+                if (in_array($request->ability, $validAbilities)) {
+                    RemovePreviousAbilties::removeAbilties($user, $model, $model_name);
+                    Bouncer::allow($user)->to($request->ability, $model);
+                }
+                if ($request->ability == "remove") {
+                    $message = "User Removed!";
+                    RemovePreviousAbilties::removeAbilties($user, $model, $model_name);
+                }
             }
-            if ($request->ability == "Read") {
-                RemovePreviousAbilties::removeAbilties($user, $model, $model_name);
-                Bouncer::allow($user)->to('read', $model);
-            } else if ($request->ability == "Can Edit") {
-                RemovePreviousAbilties::removeAbilties($user, $model, $model_name);
-                Bouncer::allow($user)->to('write', $model);
-            } else if ($request->ability == "Remove") {
-                $message = "User Removed!";
-                RemovePreviousAbilties::removeAbilties($user, $model, $model_name);
-            }
+            return redirect()->route('share-dialog', ['entity' => $entityModelSmall, 'entityId' => $request->entity_id])->with('message', $message);
+        } catch (\Exception $e) {
+            return back()->withErrors($e->getMessage());
         }
-
-        return redirect()->route('share-dialog', ['entity' => $entityModelSmall, 'entityId' => $request->entity_id])->with('message', $message);
     }
 }
